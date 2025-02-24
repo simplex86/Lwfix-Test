016ed535ae241db6be3c92da8da70ee1caf2dcde
xieheng <xieheng84@163.com>
2025-02-24 23:28:16 +0800

    const测试

--- Fixed.Net-Test/Test32/Fixed32/Numeric/ConstTest32.cs
+++ Fixed.Net-Test/Test32/Fixed32/Numeric/ConstTest32.cs
@@ -4,18 +4,29 @@ namespace Test
 {
     internal class ConstTest32 : BaseTest<Fixed32>
     {
+        private static int Zero = 0;
+        private static int One = 1;
+        private static int NegativeOne = -1;
+        private static double Half = 0.5;
+        private static int MaxValue = int.MaxValue;
+        private static int MinValue = int.MinValue;
+        private static double PI = MathF.PI;
+        private static double E = MathF.E;
+        private static double LN2 = MathF.Log(2);
+        private static double LN10 = MathF.Log(10);
+
         public override void Run()
         {
-            Assert(Fixed32.Zero, 0);
-            Assert(Fixed32.One, 1);
-            Assert(Fixed32.NegativeOne, -1);
-            Assert(Fixed32.Half, 0.5);
-            Assert(Fixed32.MaxValue, int.MaxValue);
-            Assert(Fixed32.MinValue, int.MinValue);
-            Assert(Fixed32.PI, MathF.PI);
-            Assert(Fixed32.E, MathF.E);
-            Assert(Fixed32.LN2, MathF.Log(2));
-            Assert(Fixed32.LN10, MathF.Log(10));
+            Assert(Fixed32.Zero, Zero);
+            Assert(Fixed32.One, One);
+            Assert(Fixed32.NegativeOne, NegativeOne);
+            Assert(Fixed32.Half, Half);
+            Assert(Fixed32.MaxValue, MaxValue);
+            Assert(Fixed32.MinValue, MinValue);
+            Assert(Fixed32.PI, PI);
+            Assert(Fixed32.E, E);
+            Assert(Fixed32.LN2, LN2);
+            Assert(Fixed32.LN10, LN10);
         }
     }
 }
