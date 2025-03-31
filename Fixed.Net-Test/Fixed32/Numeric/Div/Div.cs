1a91ee22733323fe8cb6d4a87d97a7d31f9801c9
xieheng <xieheng84@163.com>
2025-03-31 15:39:00 +0800

    重构除法测试

---
+++ Fixed.Net-Test/Fixed32/Numeric/Div/Div.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using Lwkit.Fixed;
+
+namespace Test
+{
+    /// <summary>
+    /// 除法 - 常规，检验准确性
+    /// </summary>
+    public class TDiv
+    {
+        private const int LOOP_TIMES = 100;
+        private const int MIN_NUMBER = int.MinValue;
+        private const int MAX_NUMBER = int.MaxValue;
+        private const int PRECISION = 5;
+
+        [Fact]
+        public void Run()
+        {
+            for (int i = 0; i < LOOP_TIMES; i++)
+            {
+                var n1 = Random.Shared.Next(MIN_NUMBER, MAX_NUMBER);
+                var n2 = Random.Shared.Next(MIN_NUMBER, MAX_NUMBER);
+                var n3 = Random.Shared.NextDouble() * n1;
+                var n4 = Random.Shared.NextDouble() * n2;
+
+                var f1 = new Fixed32(n1);
+                var f2 = new Fixed32(n2);
+                var f3 = new Fixed32(n3);
+                var f4 = new Fixed32(n4);
+
+                Assert.Equal(n1 / n3, (f1 / f3).ToDouble(), PRECISION);
+                Assert.Equal(n3 / n1, (f3 / f1).ToDouble(), PRECISION);
+                Assert.Equal(n3 / n4, (f3 / f4).ToDouble(), PRECISION);
+            }
+        }
+    }
+}
