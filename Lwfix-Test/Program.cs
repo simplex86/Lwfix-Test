0c883ecba173694c6a2006a84101e23ff7b2db07
xieheng <xieheng84@163.com>
2025-04-18 23:31:58 +0800

    删除program里的测试代码

--- Lwfix-Test/Program.cs
+++ Lwfix-Test/Program.cs
@@ -1,37 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Lwfix-Test");
-
-Console.WriteLine(double.MaxValue + 1);
-
-for (int i = 0; i < 5; i++)
-{
-    var u = System.Random.Shared.NextDouble() + System.Random.Shared.Next(int.MaxValue / 2, int.MaxValue);
-    Console.WriteLine($"{u}");
-}
-
-Console.WriteLine();
-
-for (int i = 0; i < 5; i++)
-{
-    var u = System.Random.Shared.NextDouble() + System.Random.Shared.Next(int.MinValue, int.MaxValue);
-    Console.WriteLine($"{u}");
-}
-
-Console.WriteLine();
-
-for (int i = 0; i < 5; i++)
-{
-    var u = System.Random.Shared.NextDouble() + System.Random.Shared.Next(-100, 100);
-    var v = System.Random.Shared.NextDouble() + System.Random.Shared.Next(-100, 100);
-    Console.WriteLine($"[{u}, {v}]");
-}
-
-Console.WriteLine();
-
-for (int i = 0; i < 5; i++)
-{
-    var u = System.Random.Shared.NextDouble() + System.Random.Shared.Next(-1000000, 1000000);
-    var v = System.Random.Shared.NextDouble() + System.Random.Shared.Next(-1000000, 1000000);
-    Console.WriteLine($"[{u}, {v}]");
-}
